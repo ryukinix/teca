5857c1e7f9238aebc386a42c8be3679a0f5b428f
Manoel Vilela <manoel_vilela@engineer.com>
2018-10-27 02:59:29 -0300

    Add setup.py and basic main project

---
+++ teca/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1.0'
+__author__ = ['Manoel Vilela',
+              'Samuel Hericles',
+              'Gerônimo Aguiar']
+
+__email__ = ['manoel_vilela@engineer.com',
+             'samuel.herilces@alu.ufc.br',
+              'geronimopereiraaguiar@gmail.com']

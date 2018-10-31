4791ce619a11b0badca4bc71af1880091fbe27e0
Manoel Vilela <manoel_vilela@engineer.com>
2018-10-30 23:35:41 -0300

    General refactor for user sign up screen
    CHANGELOG:
    + Split main.py in several smaller functions and procedures
    + Treat interruptions of Ctrl-C events for canceling screens
    + Query in database the available courses before showing in the menu
    + Treat telephone number: should have between 8 and 11 numbers.
    + Delete unused variables like db
    + Remove hashlib import since is unused
    + Implements menu_enumeracao function to generate a menu given a dictionary map
    + Fix a lot of errors of indentation levels

--- teca/main.py
+++ teca/main.py
@@ -3,259 +3,258 @@
 
 from teca import database
 from datetime import datetime
-import hashlib
 import getpass
+
 
 def data_valida(data_string, format='%Y-%m-%d'):
     try:
         datetime.strptime(data_string, format)
         return True
-    except:
+    except Exception:
         return False
+
+
+def menu_enumeracao(opcoes):
+    """Constroi um menu de enumeração como pergunta."""
+    for escolha, item in opcoes.items():
+        print(f'{escolha}. {item.upper()}')
+    while True:
+        op = input('>>> ')
+        if op in opcoes:
+            break
+        else:
+            print('Opção inválida')
+
+    return op
+
+
+def tela_login():
+    print('== LOGIN ==')
+    while True:
+        nickname = input('> Usuário: ')
+        senha = getpass.getpass(prompt='> Senha: ')
+        usuario = database.login(nickname, senha)
+        if usuario:
+            print('Login efetuado como: ')
+            print('Nome: ', usuario.nome.upper())
+            print('Permissão: ', usuario.permissao.upper())
+            print('Tipo de usuário: ', usuario.tipo.upper())
+            if usuario.tipo in ('aluno', 'professor'):
+                print('Curso: ', usuario.extra.nome_curso.upper())
+            break
+        else:
+            print("Usuário ou senha inválidos! Tente novamente.")
+
+
+def entrada_usuario_comum():
+    # --- INICIO DA ENTRADA DA MATRICULA --- #
+    while True:
+        matricula = input('> Digite sua matricula\n>>> ')
+        if not matricula.isdecimal():
+            print("Matricula deve ser um inteiro!")
+        elif database.Usuario.select(matricula) is None:
+            break
+        else:
+            print("Matrícula ocupada!")
+    # --- FIM DA ENTRADA DA MATRICULA --- #
+
+    while True:
+        nome = input('> Qual o seu nome completo?\n>>> ')
+        if len(nome) != 0:
+            break
+        else:
+            print('Você tem um nome! Digite.')
+
+    while True:
+        endereco = input('> Onde você mora?\n>>> ')
+        if len(endereco) != 0:
+            break
+        else:
+            print('Você mora em algum lugar!')
+
+    # ---INICIO DA ENTRADA DO TIPO DE USUARIO--- #
+    tipos = {
+        '1': 'aluno',
+        '2': 'professor',
+        '3': 'funcionario'
+    }
+    print('> Você é aluno, professor ou outro funcionário?')
+    escolha = menu_enumeracao(tipos)
+    tipo = tipos[escolha]
+
+    # --- FIM DA ENTRADA DO TIPO DE USUARIO --- #
+
+    # --- INICIO DA ENTRADA DO NICKNAME --- #
+    while True:
+        nickname = input('> Digite um nickname para acesso a sua conta: ')
+        if len(database.Usuario.filter(nickname=nickname)) == 0:
+            break
+        elif len(nickname) == 0:
+            print("Nickname não pode ser vazio!")
+        else:
+            print("Nickname já existe!")
+
+    while True:
+        senha_cadastro = getpass.getpass(prompt='> Digite uma senha: ')
+        if len(senha_cadastro) != 0:
+            break
+        else:
+            print('Senha inválida!')
+
+    # -----FIM DO CADASTRAMENTO INICAL DO USUARIO----- #
+    permissao = 'usuario'
+    senha_hash = database.senha_hash(senha_cadastro)
+    usuario = database.Usuario(matricula, nickname,
+                               senha_hash, nome,
+                               endereco, tipo, permissao)
+    return usuario
+
+
+def entrada_curso():
+    cursos = {str(c.cod_curso): c.nome_curso
+              for c in database.Curso.select_all()}
+    print('> Digite o código de seu curso')
+    cod_curso = menu_enumeracao(cursos)
+    return cod_curso
+
+
+def entrada_aluno(matricula):
+    cod_curso = entrada_curso()
+    while True:
+        print('> Em que ano, mês e dia você entrou na UFC? (YYYY-MM-DD)')
+        data_de_ingresso = input('>>> ')
+        if data_valida(data_de_ingresso):
+            break
+        else:
+            print('Data inválida!')
+    while True:
+        print('> Em que data você vai concluir seu curso? (YYYY-MM-DD)')
+        data_de_conclusao = input('>>> ')
+        if data_valida(data_de_conclusao):
+            break
+        else:
+            print("Data inválida")
+
+    # -----CHECK----- #
+    print('\n')
+    print('Código do curso: ', cod_curso)
+    print('Data de ingresso no curso: ', data_de_ingresso)
+    print('Data de conclusão do curso: ', data_de_conclusao)
+    # -----FIM DO CADASTRO DO ALUNO----- #
+
+    aluno = database.Aluno(matricula, data_de_conclusao,
+                           data_de_ingresso, cod_curso)
+    return aluno
+
+
+def entrada_professor(matricula):
+    cod_curso = entrada_curso()
+
+    while True:
+        print('> Em que data você foi contratado pela UFC? (YYYY-MM-DD)')
+        data_de_contratacao = input('>>> ')
+        if data_valida(data_de_contratacao):
+            break
+        else:
+            print("Data inválida")
+
+    tipos_regime = {
+        '1': 'DE',
+        '2': '20H',
+        '3': '40H'
+    }
+    print('> O seu regime de trabalho é de quantas horas?')
+    op = menu_enumeracao(tipos_regime)
+    regime_de_trabalho = tipos_regime[op]
+
+    # -----CHECK----- #
+    print('\n')
+    print('Data de contração: '+data_de_contratacao+'\n')
+    print('Regime de trabalho: '+regime_de_trabalho+'\n')
+    print('Código do curso: '+cod_curso+'\n')
+
+    prof = database.Professor(matricula, data_de_contratacao,
+                              regime_de_trabalho, cod_curso)
+    return prof
+
+
+def entrada_telefones(matricula):
+    telefones = []
+    while True:
+        telefone = input('> Digite um numero de telefone: ')
+
+        if not telefone.isdecimal():
+            print('Entrada inválida')
+            continue
+        elif len(str(telefone)) not in range(8, 12):
+            print('Entrada inválida')
+            continue
+
+        telefones.append(database.Telefones(matricula, telefone))
+
+        while True:
+            ask = input('> Tem mais algum telefone? (Y/N) ')
+            if ask.lower() not in ('y', 'n'):
+                print('Entrada inválida')
+            else:
+                break
+
+        if (ask.lower() == 'n'):
+            break
+
+    return telefones
+
+
+def tela_cadastro_usuario():
+    print('== CADASTRO DE USUÁRIO ==')
+
+    # Entradas para todos os tipos de usuário
+    usuario = entrada_usuario_comum()
+    telefones = entrada_telefones(usuario.matricula)
+    # Entradas especiais por tipo de usuário
+    if usuario.tipo == 'aluno':
+        aluno = entrada_aluno(usuario.matricula)
+    elif usuario.tipo == 'professor':
+        professor = entrada_professor(usuario.matricula)
+
+    confirmacao = input('\nEstá certo os dados que você inseriu? (Y/N):')
+    # -----INICIO INSERÇÃO NO BANCO DE DADOS----- #
+    if confirmacao.lower() == 'y':
+        usuario.insert()
+        for telefone in telefones:
+            telefone.insert()
+        if usuario.tipo == 'aluno':
+            aluno.insert()
+        elif usuario.tipo == 'professor':
+            professor.insert()
+        elif usuario.tipo == 'funcionario':
+            database.Funcionario(usuario.matricula).insert()
+        print('USUÁRIO CADASTRADO!')
+    # -----FIM INSERÇÃO NO BANCO DE DADOS----- #
+
 
 def main():
 
-    db = database.Database.connect()
-
- #-------------------------------------LOGIN INICIAL--------------------------------------------------                   
-
-    print("Seja bem vindo a TECA!")
-
+    # ------------LOGIN INICIAL-----------------
+    print("Seja bem-vindo a TECA! Pressione Ctrl-C para interromper a tela.")
     while True:
-        ask = input(' Já é cadastrado? Y/N ')
+        try:
+            ask = input('> Já é cadastrado? (Y/N): ')
+        except KeyboardInterrupt:
+            print("\nSaindo? Adeus então.")
+            break
 
         if (ask.lower() == 'y'):
-            nickname = input('> Usuário: ')
-            senha = getpass.getpass(prompt='>Senha: ')
-            login = database.login(nickname, senha)
-            if login:
-                print('Login efetuado como: ')
-                print('Nome: ', login.nome.upper())
-                print('Permissão: ', login.permissao.upper())
-                print('Tipo de usuário: ', login.tipo.upper())
-                break
-            else:
-                print("Usuário ou senha inválidos! Tente novamente.")   
+            try:
+                tela_login()
+            except KeyboardInterrupt:
+                print('\nLogin interrompido!')
+        elif (ask.lower() == 'n'):
+            try:
+                tela_cadastro_usuario()
+            except KeyboardInterrupt:
+                print('\nCadastro interrompido!')
 
- #-------------------------------------CADASTRAMENTO DO USUÁRIO--------------------------------------------------                   
-     
-        #-----INÍCIO DO CADASTRAMENTO INICAL DO USUARIO-----#
-        if (ask.lower() == 'n'):                 
-                print('Então vamos lhe cadastrar!!')          
-                
-                #---INICIO DA ENTRADA DA MATRICULA---#     
-                while True:
-                    matricula = input('> Digite sua matricula\n>>>: ') 
-                    if not matricula.isdecimal():
-                        print("Matricula deve ser um inteiro!")
-                    elif database.Usuario.select(matricula) is None:
-                        break
-                    else:
-                        print("Matrícula ocupada!")
-                #---FIM DA ENTRADA DA MATRICULA---#        
-
-                while True:
-                    nome = input('>Qual o seu nome completo?\n>>>: ')
-                    if len(nome) != 0:
-                        break
-                    else:
-                        print('Você tem um nome!')
-                while True:
-                    endereco = input('>Onde você mora?\n>>>: ')    
-                    if len(endereco) != 0:
-                        break
-                    else:
-                        print('Você mora em algum lugar!')
-                
-                #---INICIO DA ENTRADA DO TIPO DE USUARIO---#
-                tipos = {
-                    '1':'aluno',
-                    '2':'professor',
-                    '3':'funcionario'
-                }
-                print('> Você é aluno, professor ou outro funcionário?')
-                for numero,tipo in tipos.items():
-                    print(numero,tipo)
-                while True:
-                    tipo_usuario = input('\n>>>: ')
-                    if tipo_usuario in tipos:
-                        tipo = tipos[tipo_usuario]
-                        break
-                    else:
-                        print('Opção inválida')             
-                #---INICIO DA ENTRADA DO TIPO DE USUARIO---#
-                
-                #---INICIO DA ENTRADA DO NICKNAME---#                                   
-                while True:
-                    nickname = input('>Digite um nickname para acesso a sua conta: ')
-                    if len(database.Usuario.filter(nickname=nickname)) == 0 :
-                        break
-                    elif len(nickname) == 0:
-                        print("Nickname não pode ser vazio!")
-                    else:
-                        print("Nickname já existe!") 
-                while True:
-                    senha_cadastro = getpass.getpass(prompt='>Digite uma senha: ')
-                    if len(senha_cadastro)!=0:
-                        break
-                    else:
-                        print('Senha inválida!')
-                #---FIM DA ENTRADA DO TIPO DE USUARIO---#
-
-        #-----FIM DO CADASTRAMENTO INICAL DO USUARIO-----#
-
-
-                #-----INÍCIO DO CADASTRO DO ALUNO-----#
-                if(tipo_usuario == '1'):   
-                        
-                            #---INICIO DA ENTRADA DO CÓDIGO DE CURSO---#
-                            tipos_de_curso = {
-                                '1':'Engenharia de Computação',
-                                '2':'Engenharia Elétrica',
-                                '3':'Psicologia',
-                                '4':'Finanças',
-                                '5':'Economia'  
-                            }
-                            print('>Digite o código de seu curso')
-                            for numero,tipo in tipos_de_curso.items():
-                                print(numero,tipo)
-                            while True:
-                                cod_curso = input('\n>>>: ')
-                                if cod_curso in tipos_de_curso:
-                                    tipo = tipos_de_curso[cod_curso]
-                                    break
-                                else:
-                                    print('Opção inválida')
-                            #---FIM DA ENTRADA DO CÓDIGO DE CURSO---#
-
-                            while True:
-                                data_de_ingresso     = input('> Em que ano,mês e dia você entrou na UFC?(YYYY-MM-DD)\n>>>: ')
-                                if data_valida(data_de_ingresso):
-                                    break
-                                else:
-                                    print('Data invaláda!')
-                            while True:
-                                data_de_conclusao    = input('> Em que ano,mês e dia você vai concluir seu curso?(YYYY-MM-DD)\n>>>: ')
-                                if data_valida(data_de_conclusao):
-                                    break
-                                else:
-                                    print("Data inválida")
-                            #-----CHECK-----#
-                            print('\n')
-                            print('Código do curso: '+cod_curso+'\n')
-                            print('Data de ingresso no curso: '+data_de_ingresso+'\n')
-                            print('Data de conclusão do curso: '+data_de_conclusao+'\n')
-                #-----FIM DO CADASTRO DO ALUNO-----#            
-
-                #-----INCIO DO CADASTRO DE PROFESSOR-----#            
-                if(tipo_usuario == '2'):              
-                    while True:      
-                        data_de_contratacao  = input('> Em que ano, mês e dia você foi contratado pela UFC?(YYYY-MM-DD)\n>>>: ')
-                        if data_valida(data_de_contratacao):
-                            break
-                        else:
-                            print("Data inválida")
-                    #---INICIO DA ENTRADA DO REGIME DE TRABALHO---#
-                    tipos_regime = {
-                        '1':'D.E.(Dedicação exclusiva)',
-                        '2':'20 horas',
-                        '3':'40 horas'
-                    }
-                    print('> O seu regime de trabalho é de quantas horas?\nOpções:')
-                    for numero,regimes in tipos_regime.items():
-                        print(numero,regimes)
-                    while True:
-                        regime_trb = input('\n>>>: ')
-                        if regime_trb in tipos_regime:
-                            regimes = tipos_regime[regime_trb]
-                            if regime_trb == '1':
-                                regime_de_trabalho = 'DE'
-                                break
-                            if regime_trb == '2':
-                                regime_de_trabalho = '20H'
-                                break
-                            if regime_trb == '3':
-                                regime_de_trabalho = '40H'    
-                                break                                                                              
-                        else: 
-                            print('Opção inválida!')
-                    #---FIM DA ENTRADA DO REGIME DE TRABALHO---#        
-                    
-                    #---INICIO DA ENTRADA DO CÓDIGO DE CURSO---#    
-                    tipos_de_curso = {
-                        '1':'Engenharia de Computação',
-                        '2':'Engenharia Elétrica',
-                        '3':'Psicologia',
-                        '4':'Finanças',
-                        '5':'Economia'
-                    }
-                    print('>Digite o código de seu curso')
-                    for numero,tipo in tipos_de_curso.items():
-                        print(numero,tipo)
-                    while True:
-                        cod_curso = input('\n>>>: ')
-                        if cod_curso in tipos_de_curso:
-                            tipo = tipos_de_curso[cod_curso]
-                            break
-                        else:
-                            print('Opção inválida')
-                    #---FIM DA ENTRADA DO CÓDIGO DE CURSO---#
-
-                    #-----CHECK-----#                            
-                    print('\n')
-                    print('Data de contração: '+data_de_contratacao+'\n')
-                    print('Regime de trabalho: '+regime_de_trabalho+'\n')
-                    print('Código do curso: '+cod_curso+'\n')
-
-                #-----FIM DO CADASTRO DE PROFESSOR-----#            
-
-                #-----INICIO INSERÇÃO DOS TELEFONES-----(BUG!!!!!)(LIMITAR O TAMANHO DO TELEFONE)       
-                         
-                telefones = []
-
-                while True:
-                    telefone = input('> Digite um numero de telefone: ')
-                
-                    if not telefone.isdecimal(): 
-                        print('Entrada inválida\n')
-                    elif len(str(telefone)) != 11:
-                        print('Entrada inválida')
-             
-                    while True:
-                        ask_2 = input('> Tem mais algum telefone?(Y/N) ')
-                        if(ask_2.lower() != 'n') and (ask_2.lower() != 'y'):
-                             print('Entrada inválida')
-                        else:
-                            break        
-                    if (ask_2.lower() == 'n'):
-                        break
-                            
-                    add_telefones = telefones.append(telefone)
-                                                               
-                #-----FIM INSERÇÃO DOS TELEFONES-----#                                            
-                
-                confirmacao = input('\nEstá certo dos dados o que você colocou?(Y/N):')
-
-                #-----INICIO INSERÇÃO NO BANCO DE DADOS-----#
-                if(confirmacao.lower() == 'y'):
-
-                    database.Usuario(matricula, nickname, database.senha_hash(senha_cadastro), nome, endereco, tipo_usuario,'usuario').insert()
-                    
-                    for telefone in telefones:
-                        database.Telefones(matricula,telefone).insert()
-                    
-                    if(tipo_usuario=='1'):
-                        database.Aluno(matricula, data_de_conclusao, data_de_ingresso, cod_curso).insert()  
-
-                    elif(tipo_usuario=='2'):
-                        database.Professor(matricula, data_de_contratacao, regime_de_trabalho, cod_curso).insert()                      
-                    
-                    elif(tipo_usuario=='3'):
-                        database.Funcionario(matricula).insert()                
-                #-----FIM INSERÇÃO NO BANCO DE DADOS-----#
-
-                #-----FIM DA INSERÇÃO DE CADASTRO-----#
-                print('USUÁRIO CADASTRADO!!')
 
 if __name__ == '__main__':
     main()

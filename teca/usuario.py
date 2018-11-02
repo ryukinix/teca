b0fbac8dcd6256f888c21210eba9ddb97c45279e
Geronimo Aguiar <geron@alu.ufc.br>
2018-11-02 20:16:26 -0300

    consultas por ed, cat, titu

--- teca/usuario.py
+++ teca/usuario.py
@@ -2,7 +2,69 @@ from teca import database
 from datetime import datetime
 
 def consulta_livro():
-    pass
+    opcoes = [
+        'Pesquisar por editora',
+        'Pesquisar por categoria',
+        'Pesquisar por titulo'
+        ]
+    print("Opções: ")
+    for i, op in enumerate(opcoes):
+        print("{}. {}".format(i+1, op))
+
+    while True:
+            opcao = int(input(">>> "))
+            if opcao in range(1, len(opcoes) + 1):
+                break
+    if opcao == 1:
+        ed = input('Digite a editora: ')
+        tuplas = database.Livro.filter(editora = ed)
+        
+        for tupla in tuplas:
+            print("======================")
+            for nome_atributo in tupla._columns:
+                if nome_atributo == 'cod_categoria':
+                    print('Categoria: ', tupla.categoria)
+                else:  
+                    valor_atributo = getattr(tupla, nome_atributo)
+                    print(f"{nome_atributo}: {valor_atributo}")
+        print("======================")
+
+
+
+    elif opcao == 2:
+        cat = database.Categoria.select_all()
+        for c in cat:
+            print("======================")
+            for nome_atributo in c._columns:
+                valor_atributo = getattr(c, nome_atributo)
+                print(f"{nome_atributo}: {valor_atributo}")
+        print("======================")
+        a = input('Digite o codigo da categoria escolhida: ')
+        categoria = database.Categoria.select(a)
+        tuplas = categoria.livros
+        for tupla in tuplas:
+            print("======================")
+            for nome_atributo in tupla._columns:
+                if nome_atributo == 'cod_categoria':
+                    print('Categoria: ', tupla.categoria)
+                else:  
+                    valor_atributo = getattr(tupla, nome_atributo)
+                    print(f"{nome_atributo}: {valor_atributo}")
+        print("======================")
+
+    elif opcao == 3:
+        a = input('Digite o titulo do livro: ')
+        tuplas = database.Livro.filter(titulo = a)
+        for tupla in tuplas:
+            print("======================")
+            for nome_atributo in tupla._columns:
+                if nome_atributo == 'cod_categoria':
+                    print('Categoria: ', tupla.categoria)
+                else:  
+                    valor_atributo = getattr(tupla, nome_atributo)
+                    print(f"{nome_atributo}: {valor_atributo}")
+        print("======================")
+
 
 def ver_emprestimo(usuario):
     emprestimos = usuario.emprestimos
@@ -38,6 +100,7 @@ def tela_usuario(mat):
             'Ver emprestimos',
             'Realizar reserva',
             'Excluir cadastro',
+            'Colsultar livros',
             'Sair'
         ]
         print("Opções: ")
@@ -58,4 +121,5 @@ def tela_usuario(mat):
             if status:
                 break
         elif opcao == 4:
-            break
+            consulta_livro()
+

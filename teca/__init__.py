f621b48ac4c70e6f218aea08d4541b6f2d7283d6
Manoel Vilela <manoel_vilela@engineer.com>
2018-11-24 00:18:28 -0300

    Release da versão v1.0.0 do sistema TECA!
    Deverá ser verificado antes de merge os seguintes requisitos:
    + A aplicação está funcionando como esperado?
    + A manual foi revisado e compilado para PDF?
    + O documento descritivo com versão de compiladores e MySQL foi gerado
      em PDF?
    + Uma versão para Windows foi compilada em um executável?
    + Uma versão para Linux foi compilada em um executável?
    
    Deverá então ser realizada as operações no git para publicação da tag
    de v1.0.0 após dado merge disso como:
    
       git tag v1.0.0
       git push --tags
    
    Então será possível adicionar na seção de Downloads do bitbucket
    os outros arquivos gerados pelo código fonte como citado.

--- teca/__init__.py
+++ teca/__init__.py
@@ -6,7 +6,7 @@ realizaram o trabalho da disciplinas de banco de dados
 do semestre 2018.2
 """
 
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 __author__ = ['Manoel Vilela',
               'Samuel Hericles',
               'Gerônimo Aguiar']
